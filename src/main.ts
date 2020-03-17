1b7e67f198151fc35f86d20b56e5e28a41895c36
weite122 <949193531@qq.com>
2020-03-17 23:32:50 +0800

    [A]创建Icon组件

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ import store from './store'
 import '@/assets/styles/reset.scss';
 import Nav from '@/components/Nav.vue'
 import Layout from '@/components/Layout.vue';
-
+import Icon from '@/components/Icon.vue';
 
 Vue.config.productionTip = false;
 Vue.component('Nav', Nav)
 Vue.component('Layout', Layout);
+Vue.component('Icon', Icon);
 
 new Vue({
   router,
