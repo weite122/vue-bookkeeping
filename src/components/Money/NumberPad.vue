85e0120682944b9eb4ca08f43dd7fa26a8ed9aeb
weite122 <949193531@qq.com>
2020-03-21 16:47:04 +0800

    [A]收集四个组件的数据到record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -13,7 +13,7 @@
       <button @click="inputContent">7</button>
       <button @click="inputContent">8</button>
       <button @click="inputContent">9</button>
-      <button class="ok">OK</button>
+      <button class="ok" @click="ok">OK</button>
       <button class="zero" @click="inputContent">0</button>
       <button @click="inputContent">.</button>
     </div>
@@ -25,7 +25,8 @@
   import {Component, Prop} from 'vue-property-decorator';
   @Component
   export default class NumberPad extends Vue {
-    output = '0';
+    @Prop() readonly value!: number
+    output = this.value.toString()
     inputContent(event: MouseEvent) {
       const button = (event.target as HTMLButtonElement);
       const input = button.textContent!;
@@ -50,6 +51,9 @@
     }
     clear() {
       this.output = '0';
+    }
+    ok(){
+      this.$emit('update:value', this.output)
     }
 
   }
