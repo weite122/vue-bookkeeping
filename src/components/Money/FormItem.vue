540b104ed1b2f99865d80e2b830b3d2ad00f49fe
weite122 <949193531@qq.com>
2020-03-24 20:30:03 +0800

    [A]添加EditLabel样式

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <label class="notes">
+    <label class="formItem">
       <span class="name">{{this.fieldName}}</span>
       <input type="text"
              v-model="value"
@@ -14,7 +14,7 @@
   import {Component, Watch, Prop} from 'vue-property-decorator'
 
   @Component
-  export default class Notes extends Vue {
+  export default class FormItem extends Vue {
     value = ''
 
     @Prop({required: true}) fieldName!: string;
@@ -29,7 +29,7 @@
 </script>
 
 <style lang="scss" scoped>
-  .notes {
+  .formItem {
     font-size: 14px;
     background: #f5f5f5;
     padding-left: 16px;
@@ -39,7 +39,7 @@
       padding-right: 16px;
     }
     input {
-      height: 64px;
+      height: 40px;
       flex-grow: 1;
       background: transparent;
       border: none;
