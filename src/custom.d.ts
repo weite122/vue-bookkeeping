c576d9b04b313f696753707a7cc1b893b91604d3
weite122 <weite122@foxmail.com>
2020-03-27 15:10:12 +0800

    [A]封装record的操作

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -24,5 +24,7 @@ interface Window {
   createTag: (name: string) => void;
   findTag: (id: string) => Tag | undefined;
   removeTag: (id: string) => boolean;
-  updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+  updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+  recordList: RecordItem[];
+  createRecord: (record: RecordItem) => void;
 }
