b4676755aee707af1bfc8dda0982624eede3d2a2
weite122 <weite122@foxmail.com>
2020-03-31 22:27:21 +0800

    [A]Tags.vue使用vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -20,14 +20,16 @@
   @Component({
     computed: {
       tagList() {
-        // TODO
-        // return this.$store.fetchTags();
-        return []
+        return this.$store.state.tagList
       }
     }
   })
   export default class Tags extends Vue {
     selectedTags: string[] = [];
+
+    created(){
+      this.$store.commit('fetchTags')
+    }
 
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
@@ -44,6 +46,7 @@
       if (!name) {
         return window.alert('标签名不能为空')
       }
+      this.$store.commit('createTag',name)
     }
   }
 </script>
