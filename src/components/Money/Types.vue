48d5c0c0fc66d9d3368303504e253eda7db3d1b7
weite122 <weite122@foxmail.com>
2020-04-03 23:55:24 +0800

    [A]完善Tabs.vue,Statistics.vue,Types.vue

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="value === '-' && 'selected'" @click="selectType('-')">支出</li>
-      <li :class="value === '+' && 'selected'" @click="selectType('+')">收入</li>
+      <li :class="{[classPrefix+'-item']: classPrefix, selected: value==='-'}"
+          @click="selectType('-')">支出
+      </li>
+      <li :class="{[classPrefix+'-item']: classPrefix, selected: value==='+'}"
+          @click="selectType('+')">收入
+      </li>
     </ul>
   </div>
 </template>
@@ -13,7 +17,8 @@
 
   @Component
   export default class Types extends Vue {
-    @Prop() readonly value!: string
+    @Prop(String) readonly value!: string
+    @Prop(String) classPrefix?: string;
 
     selectType(type: string) {
       if (type !== '-' && type !== '+') {
