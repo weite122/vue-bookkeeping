ebbe4f6b29a5d1f21ffd4e10a65df5d809bf60e7
weite122 <weite122@foxmail.com>
2020-04-30 22:02:23 +0800

    deploy

---
+++ precache-manifest.1e898fa1ddb6f4935637be15e6fef792.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41007d7b130fe6ac5487",
+    "url": "/tutu-bookkeeping/css/app.94117c6a.css"
+  },
+  {
+    "revision": "9efa30407bcaa5cea789",
+    "url": "/tutu-bookkeeping/css/chunk-vendors.e11c3584.css"
+  },
+  {
+    "revision": "d58a7d569e336242e1629d150f3b47cf",
+    "url": "/tutu-bookkeeping/index.html"
+  },
+  {
+    "revision": "41007d7b130fe6ac5487",
+    "url": "/tutu-bookkeeping/js/app.f1cb9a0b.js"
+  },
+  {
+    "revision": "9efa30407bcaa5cea789",
+    "url": "/tutu-bookkeeping/js/chunk-vendors.f5748bff.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/tutu-bookkeeping/manifest.json"
+  },
+  {
+    "revision": "04bd3b7a3f41f3e93b15e1486132cdd8",
+    "url": "/tutu-bookkeeping/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tutu-bookkeeping/robots.txt"
+  }
+]);
